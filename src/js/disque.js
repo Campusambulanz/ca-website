3678818bcef8c4419d3ef28e0caaf68e613b4bb4
topada <github@topada.de>
2016-08-24 12:34:22 +0200

    disque plugin eingefügt
    disque plugin eingefügt

---
+++ src/js/disque.js
@@ -0,0 +1,10 @@
+//for documentation visit https://disqus.com/admin/universalcode/
+
+(function() {
+      var d = document, s = d.createElement('script');
+
+      s.src = '//campusambulanz2015.disqus.com/embed.js';
+
+      s.setAttribute('data-timestamp', +new Date());
+      (d.head || d.body).appendChild(s);
+})();
